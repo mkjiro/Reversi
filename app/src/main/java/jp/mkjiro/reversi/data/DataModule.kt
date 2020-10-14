eeb49d36fb6250ff8fe1b1d5c87fa3d48c8e1fc9
makoto.miura <makoto.miura@softbankrobotics.com>
2020-10-14 17:54:32 +0900

    remove no use of APIService

--- app/src/main/java/jp/mkjiro/reversi/data/DataModule.kt
+++ app/src/main/java/jp/mkjiro/reversi/data/DataModule.kt
@@ -1,11 +1,6 @@
 package jp.mkjiro.reversi.data
 
-import jp.mkjiro.reversi.data.api.NetworkModule
 import dagger.Module
 
-@Module(
-    includes = [
-        NetworkModule::class
-    ]
-)
+@Module
 class DataModule {}
