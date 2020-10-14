a469e2265bcf5add5110f51f1563df49c712bedf
mkjiro <mkjiro1017@gmail.com>
2020-10-14 17:54:32 +0900

    remove no use of APIService

--- app/src/main/java/jp/mkjiro/reversi/data/api/entity/SpreadSheet.kt
+++
@@ -1,6 +0,0 @@
-package jp.mkjiro.reversi.data.api.entity
-
-data class SpreadSheet(
-    var title: String,
-    var discript: String
-)
--- app/src/main/java/jp/mkjiro/reversi/ui/home/HomeViewModel.kt
+++ app/src/main/java/jp/mkjiro/reversi/ui/home/HomeViewModel.kt
@@ -2,7 +2,6 @@ package jp.mkjiro.reversi.ui.home
 
 import jp.mkjiro.reversi.BuildConfig
 import jp.mkjiro.reversi.base.BaseViewModel
-import jp.mkjiro.reversi.data.api.SampleAPIService
 import jp.mkjiro.reversi.ui.livedata.EventLiveData
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -12,36 +11,8 @@ import timber.log.Timber
 import javax.inject.Inject
 
 class HomeViewModel @Inject constructor(
-    val sampleAPIService: SampleAPIService
 ) : BaseViewModel<HomeEvents>() {
     override val liveEvent =
         EventLiveData<HomeEvents>()
-
-    override fun onStartWithDisposables(disposables: CompositeDisposable) {
-        super.onStartWithDisposables(disposables)
-
-
-        val path = BuildConfig.GOOGLE_PATH
-        Timber.d("%s",path)
-        val response = sampleAPIService
-            .getSpreadSheet(path)
-
-        response
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.newThread())
-            .subscribe({
-                Timber.d("HttpStatus : %s",it.code())
-                val jsonData = it.body()
-                jsonData?.forEach {
-                    Timber.d("%s", it.toString())
-                }
-            },{
-                if(it is HttpException){
-                    Timber.d("HttpStatus : %s",it.code())
-                    Timber.d("Error Message : %s", it.message())
-                }
-                Timber.e(it)
-            }).let(disposables::add)
-    }
 
 }
