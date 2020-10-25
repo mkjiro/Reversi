d197216ccaa1ddb5c719c531370addf8949bd116
mkjiro <mkjiro1017@gmail.com>
2020-10-25 21:51:06 +0900

    add turn system

--- app/src/main/java/jp/mkjiro/reversi/domain/reversi/Reversi.kt
+++ app/src/main/java/jp/mkjiro/reversi/domain/reversi/Reversi.kt
@@ -22,19 +22,18 @@ class ReversiImpl @Inject constructor(
     private val rows = 8
 
     private var board : Board = Board(columns,rows)
-    private var players = Array(2){
+    private var players = arrayOf(
         Player(
             "Black",
             Piece(PieceColor.BLACK)
-        )
+        ),
         Player(
             "White",
             Piece(PieceColor.WHITE)
         )
-    }
-    private var turnPlayer:Player = players[0]
-
-
+    )
+    private var turnPlayerIndex = 0
+    private var turnPlayer:Player = players[turnPlayerIndex]
     private val playerName : BehaviorSubject<String> by lazy{
         BehaviorSubject.create<String>()
     }
@@ -46,7 +45,8 @@ class ReversiImpl @Inject constructor(
 
     override fun reset() {
         board = Board(columns,rows)
-        turnPlayer = players[0]
+        turnPlayerIndex = 0
+        turnPlayer = players[turnPlayerIndex]
 
         board.putPiece(Coordinate(3,3), Piece(PieceColor.BLACK))
         board.putPiece(Coordinate(4,4), Piece(PieceColor.BLACK))
@@ -57,12 +57,38 @@ class ReversiImpl @Inject constructor(
     }
 
     override fun putPiece(coordinate: Coordinate) {
+        //ボードに駒を置く
         board.putPiece(coordinate,turnPlayer.piece)
+        //ひっくり返す
         ReversiLogic.getOverturnedPieces(coordinate,turnPlayer,board)
             .map {
                 board.putPiece(it,turnPlayer.piece)
             }
-        checkCellToPutPiece()
+        //ターンプレイヤーを変更
+        turnPlayer = players[++turnPlayerIndex%players.size]
+        Timber.d("%s",turnPlayer)
+        //セルの色をリセット
+        board.resetCellColor()
+        //駒が置けるかチェック
+        val cella = ReversiLogic.getCellToPutPiece(turnPlayer,board)
+        if(cella.isEmpty()){
+            //ターンプレイヤーを変更
+            turnPlayer = players[++turnPlayerIndex%players.size]
+            val cellb = ReversiLogic.getCellToPutPiece(turnPlayer,board)
+            if(cellb.isEmpty()){
+                //ゲーム終了
+            }else{
+                cella.map {
+                    Timber.d("%s",it)
+                    board.paintCell(it,CellColor.RED)
+                }
+            }
+        }else{
+            cella.map {
+                Timber.d("%s",it)
+                board.paintCell(it,CellColor.RED)
+            }
+        }
         playerName.onNext(turnPlayer.name)
     }
 
