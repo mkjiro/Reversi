599f116e47c4aa0e84cb8236d5fe193d44ba46c9
mkjiro <mkjiro1017@gmail.com>
2020-11-27 10:49:21 +0900

    add buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlin_version by extra("1.3.72")
-    val ktlint_version by extra("9.4.1")
-    val kotlin_coroutines_version by extra("1.1.1")
-    val work_version by extra("2.0.1")
-    val dagger_version by extra("2.23.1")
-    val support_version by extra("1.0.2")
-    val navigation_version by extra("2.2.2")
-    val rxandroid_version by extra("2.1.1")
-    val rxkotlin_version by extra("2.3.0")
-    val retrofit_version by extra("2.8.1")
-    val okhttp_version by extra("4.6.0")
-    val glide_version by extra("4.10.0")
-    val junit_version by extra("5.7.0")
-
     repositories {
         google()
         jcenter()
         maven(url = "https://plugins.gradle.org/m2/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.0")
-        classpath(kotlin("gradle-plugin", version = kotlin_version))
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navigation_version")
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:9.4.1")
+        classpath(deps.Libs.GradlePlugin.android)
+        classpath(deps.Libs.GradlePlugin.kotlin)
+        classpath(deps.Libs.GradlePlugin.safeArgs)
+        classpath(deps.Libs.GradlePlugin.ktlint)
     }
-
 }
 
 allprojects {
---
+++ buildSrc/src/main/java/deps/Libs.kt
@@ -0,0 +1,67 @@
+package deps
+
+object Libs{
+    object GradlePlugin{
+        const val android = "com.android.tools.build:gradle:4.1.0"
+        const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72"
+        const val safeArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:2.2.2"
+        const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.4.1"
+    }
+
+    object Test {
+        const val junitAPI = "org.junit.jupiter:junit-jupiter-api:5.6.1"
+        const val junitEngine = "org.junit.jupiter:junit-jupiter-engine"
+        const val testRunner = "androidx.test:runner:1.2.0"
+    }
+
+    object Timber {
+        const val client = "com.jakewharton.timber:timber:4.7.1"
+    }
+
+    object Kotlin {
+        const val stdlibJvm = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.72"
+    }
+
+    object AndroidX {
+        const val appCompat = "androidx.appcompat:appcompat:1.1.0"
+        const val recyclerView = "androidx.recyclerview:recyclerview:1.1.0"
+        const val gridLayout = "androidx.gridlayout:gridlayout:1.0.0"
+        const val constraint = "androidx.constraintlayout:constraintlayout:1.1.3"
+        const val constraintSolver = "androidx.constraintlayout:constraintlayout-solver:1.1.3"
+
+        const val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:2.2.0"
+        const val lifecycleCompiler = "androidx.lifecycle:lifecycle-compiler:2.2.0"
+
+        object Navigation {
+            const val fragmentKtx = "androidx.navigation:navigation-fragment-ktx:2.2.0"
+            const val uiKtx = "androidx.navigation:navigation-ui-ktx:2.2.0"
+        }
+    }
+
+    object RxJava{
+        const val client = "io.reactivex.rxjava2:rxandroid:2.1.1"
+    }
+
+    object Dagger {
+        const val client = "com.google.dagger:dagger:2.23.1"
+        const val compiler = "com.google.dagger:dagger-compiler:2.23.1"
+        const val android = "com.google.dagger:dagger-android:2.23.1"
+        const val support = "com.google.dagger:dagger-android-support:2.23.1"
+        const val processor = "com.google.dagger:dagger-android-processor:2.23.1"
+    }
+
+    object OkHttp{
+        const val client = "com.squareup.okhttp3:okhttp:4.9.0"
+    }
+
+    object Retrofit {
+        const val client = "com.squareup.retrofit2:retrofit:2.9.0"
+        const val rxjava = "com.squareup.retrofit2:adapter-rxjava2:2.9.0"
+        const val gson = "com.squareup.retrofit2:converter-gson:2.3.0"
+    }
+
+    object Gson {
+        const val client = "com.google.code.gson:gson:2.8.6"
+    }
+
+}
